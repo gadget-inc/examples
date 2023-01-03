39d73e919d65724c10daba7328c210abef31a936
Harry Brundage <harry.brundage@gmail.com>
2023-01-03 14:25:05 -0500

    Make the Shopify CLI example product manager no longer rely on CUD Gadget action

--- packages/shopify-cli-embedded/web/frontend/components/ProductManager.jsx
+++ packages/shopify-cli-embedded/web/frontend/components/ProductManager.jsx
@@ -1,33 +1,29 @@
-import { useAction, useFindMany } from "@gadgetinc/react";
+import { useFindMany } from "@gadgetinc/react";
 import { api } from "../api";
-import { Button, Card, Layout, Stack } from "@shopify/polaris";
+import { Card, Layout, Spinner, Stack } from "@shopify/polaris";
 
 export function ProductManager() {
-  const [_, deleteProduct] = useAction(api.shopifyProduct.delete);
-  const [{ data, fetching, error }, refresh] = useFindMany(api.shopifyProduct);
+  const [{ data, fetching, error }, _refresh] = useFindMany(api.shopifyProduct);
 
   if (error) return <>Error: {error.toString()}</>;
 
   return (
     <Layout>
       <Layout.Section>
-        {fetching && <span>Loading...</span>}
+        {fetching && <Spinner />}
         {!fetching &&
           data.map((product) => (
-            <Card sectioned key={product.title}>
+            <Card sectioned key={product.title} title={product.title}>
               <Stack alignment="center">
-                <Stack.Item>{product.title}</Stack.Item>
-                <Button
-                  onClick={() => {
-                    void deleteProduct({ id: product.id }).then(() => refresh());
-                  }}
-                >
-                  Delete {product.title || "No title found"}
-                </Button>
+                <Stack.Item>ID: {product.title}</Stack.Item>
               </Stack>
             </Card>
           ))}
-        {!fetching && data.length == 0 && <Card>No products found</Card>}
+        {!fetching && data.length == 0 && (
+          <Card sectioned>
+            <p>No products found</p>
+          </Card>
+        )}
       </Layout.Section>
     </Layout>
   );
