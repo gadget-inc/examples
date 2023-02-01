29cec4f144d0b619072aed16141244a7785dc2ce
rdraward <riley@gadget.dev>
2023-02-01 17:42:13 -0500

    fix frontend ports and update to latest CLI version for example apps

--- packages/product-recommendations-quiz-app/product-quiz-admin/web/frontend/vite.config.js
+++ packages/product-recommendations-quiz-app/product-quiz-admin/web/frontend/vite.config.js
@@ -5,35 +5,32 @@ import react from "@vitejs/plugin-react";
 
 if (process.env["npm_lifecycle_event"] === "build" && !process.env["CI"] && !process.env["SHOPIFY_API_KEY"]) {
   console.warn(
-    "Building the frontend app without an API key. The frontend build will not run without an API key. Set the SHOPIFY_API_KEY environment variable when running the build command."
+    "\nBuilding the frontend app without an API key. The frontend build will not run without an API key. Set the SHOPIFY_API_KEY environment variable when running the build command.\n"
   );
 }
 
-const FRONTEND_PORT = 3000;
-
-let hmrConfig = {
-  protocol: "ws",
-  host: "localhost",
-  port: FRONTEND_PORT,
-  clientPort: FRONTEND_PORT,
-};
+const host = "localhost";
+const port = 3005;
 
 export default defineConfig({
   root: dirname(fileURLToPath(import.meta.url)),
   plugins: [react()],
   define: {
-    process: {
-      env: {
-        SHOPIFY_API_KEY: process.env["SHOPIFY_API_KEY"],
-      },
-    },
+    "process.env": JSON.stringify({
+      SHOPIFY_API_KEY: process.env["SHOPIFY_API_KEY"],
+    }),
   },
   resolve: {
     preserveSymlinks: true,
   },
   server: {
-    host: "localhost",
-    port: FRONTEND_PORT,
-    hmr: hmrConfig,
+    host: host,
+    port: port,
+    hmr: {
+      protocol: "ws",
+      host: host,
+      port: port,
+      clientPort: port,
+    },
   },
 });
